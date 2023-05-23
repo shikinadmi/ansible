ebd43d4489e99907a9a0c0abc95c7d1f65dc3eaa
Шикин Александр <shikin.a.dmi@icloud.com>
2023-05-23 13:46:30 +0300

    readme best practics

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 ansible "$1" -m ping && ansible-playbook playbook.yml -i inventories/"$1" -e HOSTS="$1"
 
+
 # Описание запуска
 # ./run.sh [inventorry name]
 # где inventorry name - имя нашего инвентарного файла, который должен находиться в папке inventories
