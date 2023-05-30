576017367f529be386a1ec7f6799fc334127bc6d
Шикин Александр <shikin.a.dmi@icloud.com>
2023-05-30 23:56:49 +0300

    refactor

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/zsh
-ansible "$1" -m ping && ansible-playbook playbook.yml -i inventories/"$1" -e HOSTS="$1"
+inventory_file="inventories/$1"
+if ! ansible "$1" -m ping &> /dev/null; then
+  echo "Failed to ping $1"
+  exit 1
+fi
+ansible-playbook playbook.yml -i "$inventory_file" -e HOSTS="$1" --limit "$1" -f 10 # run with 10 forks
+
+
 
 
 # Описание запуска
