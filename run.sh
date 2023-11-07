f44f6eb04d4053a6f06b993286dbc65a80ed0c46
Shikin Aleksandr <shikin.a.dmi@icloud.com>
2023-11-07 15:30:46 +0300

    defaults | roles

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ if ! ansible "$1" -m ping &> /dev/null; then
   echo "Failed to ping $1"
   exit 1
 fi
-ansible-playbook playbook.yml -i "$inventory_file" -e HOSTS="$1" --limit "$1" -f 10 # run with 10 forks
+ansible-playbook playbook.yml -i "$inventory_file" -e HOSTS="$1" -f 10 # run with 10 forks
+#ansible-playbook playbook.yml -i "$inventory_file" -e HOSTS="$1" --limit "$1" -f 10 # run with 10 forks
 
 
 
