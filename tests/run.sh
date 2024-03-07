d69ff5bf4ba10c4a3725fe664af7c58216d2fb11
Шикин Александр <shikin.a.dmi@icloud.com>
2024-03-07 16:42:06 +0300

    review

--- tests/run.sh
+++ tests/run.sh
@@ -5,7 +5,10 @@ if ! ansible "$1" -m ping &> /dev/null; then
   echo "Failed to ping $1"
   exit 1
 fi
-ansible-playbook playbook.yml -i hosts -e HOSTS="$1" -f 10 -v # run with 10 forks
+# Проверка возможности запуска ansible-playbook через ansible-lint
+#ansible-lint playbook.yml -e HOSTS="$1" -v # run with 10 forks
+ansible-playbook playbook.yml -i hosts -e HOSTS="$1" -f 10 #-v # run with 10 forks
+
 #ansible-playbook playbook.yml -i "$inventory_file" -e HOSTS="$1" --limit "$1" -f 10 # run with 10 forks
 
 
