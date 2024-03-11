38cb554e082f0daf82c9e4dfd92bf746a0fade2c
Шикин Александр <shikin.a.dmi@icloud.com>
2024-03-11 23:19:08 +0300

    review | recreate schemas | close session user after drop

--- tests/run.sh
+++ tests/run.sh
@@ -5,8 +5,6 @@ if ! ansible "$1" -m ping &> /dev/null; then
   echo "Failed to ping $1"
   exit 1
 fi
-# Проверка возможности запуска ansible-playbook через ansible-lint
-#ansible-lint playbook.yml -e HOSTS="$1" -v # run with 10 forks
 ansible-playbook playbook.yml -i hosts -e HOSTS="$1" -f 10 #-v # run with 10 forks
 
 #ansible-playbook playbook.yml -i "$inventory_file" -e HOSTS="$1" --limit "$1" -f 10 # run with 10 forks
