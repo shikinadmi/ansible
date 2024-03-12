d8fa723c91a946a2771ec32395407312ccb51044
Шикин Александр <shikin.a.dmi@icloud.com>
2024-03-12 12:39:27 +0300

    review | recreate schemas | close session user after drop | drop files age 2m

--- tests/run.sh
+++ tests/run.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 # shellcheck disable=SC2034
 #inventory_file="inventories/$1"
-if ! ansible "$1" -m ping &> /dev/null; then
-  echo "Failed to ping $1"
-  exit 1
-fi
+#if ! ansible "$1" -m ping &> /dev/null; then
+#  echo "Failed to ping $1"
+#  exit 1
+#fi
 ansible-playbook playbook.yml -i hosts -e HOSTS="$1" -f 10 #-v # run with 10 forks
 
 #ansible-playbook playbook.yml -i "$inventory_file" -e HOSTS="$1" --limit "$1" -f 10 # run with 10 forks
